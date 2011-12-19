4da82fad383b4d871878254aad2ab0a0bd2aa402
Kree Cole-McLaughlin <kree.colemclaughlin@gmail.com>
2011-12-19 15:52:40 -0800

    added gradient operator calculation.

--- src/image_vector.hpp
+++ src/image_vector.hpp
@@ -41,12 +41,52 @@ struct image_channel_to_vector
   size_t m_chan;
 };
 
+struct vector_to_image_channel
+{
+  typedef void result_type;
 
-vector_t nth_channel_vector( const image_t& image, size_t n )
+  vector_to_image_channel( const vector_t& u, size_t chan )
+    : m_chan( chan ),
+      m_vec( u )
+  {}
+
+  template< class ImageView >
+  result_type operator() ( ImageView& view )
+  {
+    typedef typename boost::gil::nth_channel_view_type< ImageView >::type
+      ChannelView;
+
+    typedef typename ChannelView::iterator Iter;
+    
+    ChannelView chan_view = boost::gil::nth_channel_view( view, m_chan );
+
+    size_t w = chan_view.width();
+    size_t h = chan_view.height();
+    
+    size_t i = 0;
+    for( Iter pix = chan_view.begin(); pix != chan_view.end(); ++pix )
+      (*pix)[0] = m_vec[ i++ ];
+  }
+
+  size_t    m_chan;
+  vector_t  m_vec;
+};
+
+
+vector_t nth_channel_vector( const image_t& image, size_t n = 0 )
 {
   using namespace boost::gil;
   
   return apply_operation( const_view( image ), image_channel_to_vector( n ) );
+}
+
+
+template< typename ImageView >
+void nth_channel_from_vector( const vector_t& u, ImageView& image, size_t n = 0 )
+{
+  using namespace boost::gil;
+  
+  return apply_operation( view( image ), vector_to_image_channel( u, n ) );
 }
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,28 @@
 
 #include "image_types.hpp"
 #include "image_io.hpp"
+#include "image_vector.hpp"
+#include "image_grad.hpp"
 
 namespace opts = boost::program_options;
 
 int main( int argc, char* argv[] )
 {
+  // input
   std::string input_path = "";
   
+  // gradient computation
+  bool compute_grad = false;
+  std::string grad_x_path = "";
+  std::string grad_y_path = "";
+
   opts::options_description options( "Usage:" );
   options.add_options()
     ( "help", "Print usage information" )
     ( "input,i", opts::value<std::string>(),
-      "Path to input image." );
+      "Path to input image." )
+    ( "grad,g",
+      "Compute the gradient of the image." );
   
   opts::variables_map vm;
   opts::store( opts::parse_command_line( argc, argv, options ), vm );
@@ -35,6 +45,19 @@ int main( int argc, char* argv[] )
   if( vm.count("input") ) 
     input_path = vm[ "input" ].as<std::string>();
 
+  if( vm.count("grad") ) 
+    {
+      compute_grad = true;
+
+      size_t pos = input_path.find_last_of( '.' );
+
+      grad_x_path = input_path.substr( 0, pos )
+        + "_dx" + input_path.substr( pos );
+
+      grad_y_path = input_path.substr( 0, pos )
+        + "_dy" + input_path.substr( pos );
+    }
+
   std::cout << "=======  ImageDEC =======\n";
 
   image_t input_image;  
@@ -47,6 +70,30 @@ int main( int argc, char* argv[] )
                 << input_image.num_channels() << " channel, "
                 << input_image.width() << " x "
                 << input_image.height() << " successfully read.\n";
+    }
+
+  if( compute_grad )
+    {
+      const size_t w = input_image.width();
+      const size_t h = input_image.height();
+      
+      image_grad_op grad( w, h );
+      
+      vector_t u  = nth_channel_vector( input_image, 0 );
+      vector_t du = grad * u;
+      
+      using mtl::irange;
+
+      const size_t mid = ( w-1 ) * h;
+      const size_t end = du.size();
+      
+      vector_t u_x = du[ irange( 0, mid ) ];
+      vector_t u_y = du[ irange( mid, end ) ];
+      
+      gray8_image_t du_x( w-1, h );
+      gray8_image_t du_y( w, h-1 );
+      
+      
     }
   
   return 0;
